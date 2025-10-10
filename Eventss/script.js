12fb9d3dda89e29c274dbce248a3b5ea5e1824d0
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-10-11 00:53:46 +0530

    first init

--- Eventss/script.js
+++ Eventss/script.js
@@ -1,3 +1,15 @@
-document.querySelector('#images').addEventListener('clcik' , function(e){
-    console.log("clicked on owl");
-});
+// setTimeout(function(){
+//     console.log("Aakarsh")
+// },2000)
+// // setInterval(() => {
+// //     console.log("Aakarsh")
+    
+// }, );
+
+const saytext = function(){
+    console.log("Aakarsh")
+}
+const changetext = function(){
+    document.querySelector('h2').innerHTML="best javascript course batch"
+}
+ setTimeout(changetext,2000)
