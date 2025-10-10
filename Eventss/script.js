063596a0b6e8ff05cb2451399a94a77b1011d1d8
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-10-11 01:49:46 +0530

    updating my html and js code stop start qns

--- Eventss/script.js
+++ Eventss/script.js
@@ -1,19 +1,22 @@
-setTimeout(function(){
-    console.log("Aakarsh")
-},2000)
-// setInterval(() => {
-//     console.log("Aakarsh")
-    
-
-
-const saytext = function(){
-    console.log("Aakarsh")
-}
-const changetext = function(){
-    document.querySelector('h2').innerHTML="best javascript course batch"
-}
- const chnage =setTimeout(changetext,2000)
- document.querySelector('#stop').addEventListener('click' , function(){
-    clearTimeout(chnage)
-    console.log("Stopped")
- })
+// setInterval(function(){
+//     console.log("Aakarsh" , Date.now())
+// },1000)
+// const interval = function(){
+//     console.log("Aakarsh" , Date.now())
+// }
+// setInterval(interval,1000)
+const hell = document.querySelector('#startButton')
+const hello = document.querySelector('#stopButton')
+// const interval = setInterval(function(){
+//     console.log("Start Printing" , Date.now())
+// },1000)
+hell.addEventListener('click', function(){
+   interval = setInterval(function(){
+        console.log("start printing" , Date.now())
+    },1000)
+   
+})
+hello.addEventListener('click',function(){
+  clearInterval(interval)
+  console.log("Stop Printing",Date.now())
+})
