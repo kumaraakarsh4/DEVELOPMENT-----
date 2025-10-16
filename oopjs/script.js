5f20ed6bc0e3ae67e5d938effe3689cedcf37090
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-10-17 01:26:45 +0530

    prototype code

--- oopjs/script.js
+++ oopjs/script.js
@@ -10,22 +10,33 @@
 // console.log(user.username);
 // console.log(user.getUserDetails());
 
-function user(username,logincount,signedIn){
+// function user(username,logincount,signedIn){
     //this.username --> variable ; username is we passing the value
      
-    this.username = username;
-    this.logincount = logincount;
-    this.signedIn = signedIn;
-    this.greeting = function(){
-        console.log(`welcome ${this.username}`);
+//     this.username = username;
+//     this.logincount = logincount;
+//     this.signedIn = signedIn;
+//     this.greeting = function(){
+//         console.log(`welcome ${this.username}`);
         
-    }
-    return this
+//     }
+//     return this
+// }
+// const userone = new user("Aakarsh", 8 , true);
+// const usertwo = new user("Dimpu" , 11 , false);
+// console.log(userone.constructor);
+// console.log(usertwo);
+
+function multiple(num){
+    return num*5;
 }
-const userone = new user("Aakarsh", 8 , true);
-const usertwo = new user("Dimpu" , 11 , false);
-console.log(userone);
-console.log(usertwo);
+multiple.power = 2;
+console.log(multiple(5));
+console.log(multiple.power);
+console.log(multiple.prototype);
+
+
+
 
 
 
