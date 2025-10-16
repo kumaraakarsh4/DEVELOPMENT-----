1bf7a79c326d23e2b4ca8acd867bf5eedbeb3deb
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-10-17 00:50:30 +0530

    fourth init

--- oopjs/script.js
+++ oopjs/script.js
@@ -1,12 +1,27 @@
-const user ={
-    username : "Aakarsh",
-    logincount : 8,
-    signedIn : true,
-    getUserDetails: function(){
-        console.log(`username:${this.username}`);
+// const user ={
+//     username : "Aakarsh",
+//     logincount : 8,
+//     signedIn : true,
+//     getUserDetails: function(){
+//         console.log(`username:${this.username}`);
         
-    }
+//     }
+// }
+// console.log(user.username);
+// console.log(user.getUserDetails());
+
+function user(username,logincount,signedIn){
+    //this.username --> variable ; username is we passing the value
+     
+    this.username = username;
+    this.logincount = logincount;
+    this.signedIn = signedIn;
+    return this
 }
-console.log(user.username);
-console.log(user.getUserDetails());
+const userone = new user("Aakarsh", 8 , true);
+const usertwo = new user("Dimpu" , 11 , false);
+console.log(userone);
+console.log(usertwo);
+
+
 
