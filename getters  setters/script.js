129201d3a2d269d13ee665d3c6dace633df29314
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-10-26 15:23:51 +0530

    first init

--- getters  setters/script.js
+++ getters  setters/script.js
@@ -1,22 +1,32 @@
-class user{
-    constructor(email , password){
-        this.email = email;
-        this.password = password
-    }
-    get email(){
-        return this.emails
-    }
-    set email(value){
-        this.emails = value.toUpperCase()
-    }
-    get password(){
-        return `${this.passwords}aakarsh`.toUpperCase()
-    }
-    set password(value){
-        this.passwords = value.toUpperCase()
-    }
-}
-const print = new user("aakarsh@jio.com" , "abc")
-console.log(print.password);
-console.log(print.email);
+// class user{
+//     constructor(email , password){
+//         this.email = email;
+//         this.password = password
+//     }
+//     get email(){
+//         return this.emails
+//     }
+//     set email(value){
+//         this.emails = value.toUpperCase()
+//     }
+//     get password(){
+//         return `${this.passwords}aakarsh`.toUpperCase()
+//     }
+//     set password(value){
+//         this.passwords = value.toUpperCase()
+//     }
+// }
+// const print = new user("aakarsh@jio.com" , "abc")
+// console.log(print.password);
+// console.log(print.email);
 
+function init(){
+    let name = "Morzilla";
+    function displayname(){
+        console.log(name);
+
+        
+    }
+    displayname();
+}
+init();
